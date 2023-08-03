b4551bbdd9d476d08dd367b749c10ab11031acd0
Deepak <Deepak@TYSS-Deepak>
2023-08-03 12:10:24 +0530

    Product , Added in POM class

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+	
+	@FindBy(xpath = "//input[@alt='Create Product...']")
+	private WebElement createProductImgBtn;
+
+}
